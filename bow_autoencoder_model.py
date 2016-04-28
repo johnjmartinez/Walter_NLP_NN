e16ad477a703f2422057e1fc0fe334eab8663200
johnjmartinez@utexas.edu <johnjmartinez@utexas.edu>
2016-04-28 02:11:44 -0500

    tweaked model and corpus creation to account for field mapping separation ... crossing fingers

--- bow_autoencoder_model.py
+++ bow_autoencoder_model.py
@@ -7,12 +7,11 @@ import theano.tensor as TT
 import numpy as np
 import json, time
 
-
 class BOWLoss(Loss):
     def __call__(self, outputs):
         output = outputs[self.output_name]
         target = self._target
-        xe = target * TT.log(output) + (1 - target) * TT.log(1 - output) # Cross-entropy
+        xe = target * TT.log(output) + (1 - target) * TT.log(1 - output)  # Cross-entropy
         return -xe.sum()
 
 class Corpus:
@@ -67,8 +66,8 @@ def simpleTranslate(sentences, translationMap):
     return translatedSentences
 
 def getTime():
-     localtime = time.asctime( time.localtime(time.time()) )
-     return localtime
+    localtime = time.asctime( time.localtime(time.time()) )
+    return localtime
 
 
 """
@@ -104,84 +103,81 @@ englishCorpus = Corpus(englishSentences)
 alieneseSentences = simpleTranslate(englishSentences, englishToAlieneseTranslationMap)
 alieneseCorpus = Corpus(alieneseSentences)
 
-englishCorpus[0].printToConsole()
-alieneseCorpus[0].printToConsole()
-
 """
 
-
-#REAL DATA 
+#REAL DATA
 englishCorpus = []
 alieneseCorpus = []
 field = []
-print "Opening map"
-with open('query2doc2field_map.txt') as data_file:    
+print "Opening map\n"
+with open('query2doc2field_map.txt') as data_file:
     data = json.load(data_file)
 
 for f in data.keys():
-	
-	field.append(f)
 
-	print "Creating source nodes corpus for %s"%f
-	x=Corpus(data[f].keys())
-	englishCorpus.append(x)
-	print " .... %s words" % x.wordCount()
+    field.append(f)
 
-	print "Creating target nodes corpus for %s"%f 
-	alien = []   
-	for val in data[f].values():
-		a = ' '.join(list(set(val)))
-		alien.append(a)
-	y=Corpus(alien)
-	alieneseCorpus.append(y)
-	print " .... %s words" % y.wordCount()
+    print "Creating source nodes corpus for %s"%f
+    x=Corpus(data[f].keys())
+    englishCorpus.append(x)
+    print " .... %s words" % x.wordCount()
 
-exit
+    print "Creating target nodes corpus for %s"%f
+    alien = []
+    for val in data[f].values():
+        a = ' '.join(list(set(val)))  #reduce common lines and join them all in L
+        a = ' '.join(list(set(a.split())))  #reduce common words in L
+        alien.append(a)
+    y=Corpus(alien)
+    alieneseCorpus.append(y)
+    print " .... %s words\n" % y.wordCount()
+
 #HERE GOES NOTHING
 ae = []
-print "Creating ae model"   
-ae[0] = Network(
-    [
+field=[field[0]] #temp solution to out of mem error
+for f in field:
+    print "Creating ae %s model"%f
+    ae.append( Network([
         englishCorpus[0].wordCount(),
-        ((englishCorpus[0].wordCount() + alieneseCorpus[0].wordCount())) / 2,
+        ((englishCorpus[0].wordCount() + alieneseCorpus[0].wordCount())) * 2,
         (alieneseCorpus[0].wordCount(), 'sigmoid')
-    ],
-    loss='bowloss'
-)
- 
-modelPath = "./bowloss_nag_mmntm1_loss001_gpu.mod"
+        ], loss='bowloss') )
+
+modelPath = "./bow_nag_m1_loss001_gpu.%s.mod"%field[0]
 # if path.isfile(modelPath):
-#	ae.load(modelPath)
+#	ae[0].load(modelPath)
 # else:
-# ae.train([np.asarray(englishCorpus[0].wordVectors), np.asarray(alieneseCorpus[0].wordVectors)], algo='sgd')
-#	ae.save(modelPath)
+# ae[0].train([np.asarray(englishCorpus[0].wordVectors), np.asarray(alieneseCorpus[0].wordVectors)], algo='sgd')
+#	ae[0].save(modelPath)
 
 print "Training ae model %s" % modelPath
 print " - start time: %s" % getTime()
 x=1
 target_loss =  0.001
 while x > target_loss:
-    for train, valid in ae.itertrain([np.asarray(englishCorpus[0].wordVectors,  dtype=np.float32), 
-                                      np.asarray(alieneseCorpus[0].wordVectors, dtype=np.float32)], 
+    for train, valid in ae[0].itertrain([np.asarray(englishCorpus[0].wordVectors,  dtype=np.float32),
+                                      np.asarray(alieneseCorpus[0].wordVectors, dtype=np.float32)],
                                       algo='nag', momentum=1):
-                                    
+
         if train['loss'] < target_loss: break # NOT RELIABLE ... MAY STOP TRAINING WAY BEFORE TARGET LOSS
-    
+
     print "  time: %s" % getTime()
     print "  training loss: %s" % train['loss']
-    print "  validation loss: %s\n" % valid['loss']
+    print "  validation loss: %s" % valid['loss']
     x = train['loss']
+    print "  Saving ae %s model" % field[0]
+    ae[0].save(modelPath)
 
-print " - stop time: %s\n" % getTime()     
-print "Saving ae model"       
-ae.save(modelPath)
+print " - stop time: %s\n" % getTime()
+print "Saving ae %s model" % field[0]
+ae[0].save(modelPath)
 # for wordVector in englishCorpus[0].wordVectors:
-#	print alieneseCorpus[0].convertFromWordVector(ae.predict(np.asarray([wordVector]))[0].tolist())
+#	print alieneseCorpus[0].convertFromWordVector(ae[0].predict(np.asarray([wordVector]))[0].tolist())
 
-print "Testing ae model"   
-for word in englishCorpus[0].vocabulary: 
+print "Testing ae model"
+for word in englishCorpus[0].vocabulary:
     englishWordVector = englishCorpus[0].convertToWordVector([word])
-    alienWordVector = ae.predict(np.asarray([englishWordVector], dtype=np.float32))[0]
+    alienWordVector = ae[0].predict(np.asarray([englishWordVector], dtype=np.float32))[0]
     # highestProbabilityIndex = np.argmax(alienWordVector)
     sortedIndices = np.argsort(alienWordVector)[::-1]
     print "%s: %s" % (word, np.asarray(alieneseCorpus[0].vocabulary)[sortedIndices[0:2]])
